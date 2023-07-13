6550c6d6632e623142a7d6bee8426662b3259851
jdtheefirst <124874019+jdtheefirst@users.noreply.github.com>
2023-07-13 16:14:22 +0300

    Update ProfileModal.js

--- frontend/src/components/miscellanious/ProfileModal.js
+++ frontend/src/components/miscellanious/ProfileModal.js
@@ -13,49 +13,63 @@ import {
   Text,
   Image,
 } from "@chakra-ui/react";
+import { useState } from "react";
 
 const ProfileModal = ({ user, children }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isFocused, setIsFocused] = useState(false);
+
+  const toggleFocus = () => {
+    setIsFocused((prevState) => !prevState);
+  };
 
   return (
     <>
       {children ? (
         <span onClick={onOpen}>{children}</span>
       ) : (
-        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+        />
       )}
       <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
         <ModalOverlay />
-        <ModalContent h="410px">
+        <ModalContent height="410px">
           <ModalHeader
             fontSize="40px"
             fontFamily="Work sans"
-            d="flex"
+            display="flex"
             justifyContent="center"
           >
             {user.name}
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody
-            d="flex"
+            display="flex"
             flexDir="column"
             alignItems="center"
             justifyContent="space-between"
           >
             <Image
               borderRadius="full"
-              boxSize="150px"
+              boxSize={isFocused ? "300px" : "150px"}
               src={user.pic}
               alt={user.name}
+              cursor="pointer"
+              onClick={toggleFocus}
+              transition="box-size 0.3s ease-in-out"
             />
             <Text
               fontSize={{ base: "28px", md: "30px" }}
               fontFamily="Work sans"
+              display={isFocused ? "none" : "flex"}
             >
               Email: {user.email}
             </Text>
           </ModalBody>
-          <ModalFooter>
+          <ModalFooter display={isFocused ? "none" : "flex"}>
             <Button onClick={onClose}>Close</Button>
           </ModalFooter>
         </ModalContent>
