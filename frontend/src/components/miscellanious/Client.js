87bd8b170e5bba03fbcc83915047060a67c5c6af
jose <almanobe2@gmail.com>
2023-07-29 18:58:39 +0300

    client

---
+++ frontend/src/components/miscellanious/Client.js
@@ -0,0 +1,84 @@
+import { ViewIcon } from "@chakra-ui/icons";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  IconButton,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import { ChatState } from "../Context/ChatProvider";
+
+const ClientModal = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isFocused, setIsFocused] = useState(false);
+  const { user } = ChatState();
+
+  const toggleFocus = () => {
+    setIsFocused((prevState) => !prevState);
+  };
+
+  return (
+    <>
+      {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton
+          display={{ base: "flex" }}
+          icon={<ViewIcon />}
+          onClick={onOpen}
+        />
+      )}
+      <Modal size="lg" onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent height="410px">
+          <ModalHeader
+            fontSize="40px"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent="center"
+          >
+            {user.name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Image
+              borderRadius="full"
+              boxSize={isFocused ? "300px" : "150px"}
+              src={user.pic}
+              alt={user.name}
+              cursor="pointer"
+              onClick={toggleFocus}
+              transition="box-size 0.3s ease-in-out"
+            />
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Work sans"
+              display={isFocused ? "none" : "flex"}
+            >
+              Email: {user.email}
+            </Text>
+          </ModalBody>
+          <ModalFooter display={isFocused ? "none" : "flex"}>
+            <Button>Edit</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default ClientModal;
