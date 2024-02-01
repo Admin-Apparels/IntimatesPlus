d270f4ac150110b665a2f8a6216a6fe91d1f0feb
jdtheefirst <jngatia045@gmail.com>
2024-02-01 00:23:53 -0900

    good

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -137,7 +137,8 @@ function Homepage() {
               m={0}
             >
               <Text
-                fontWeight="bold"
+                fontSize={{ base: "medium", md: "bold" }}
+                width={"100%"}
                 p={0}
                 m={0}
                 userSelect={"none"}
